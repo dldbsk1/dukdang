3083968ca00f10dacab69beb7ffcf594a788f9b9
mungppuccino <mungppuccino@gmail.com>
2026-03-26 22:32:47 +0900

    Delete backend directory

--- backend/src/main/java/com/example/dukdang/dto/AuctionBidRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.dukdang.dto;
-
-import lombok.Getter;
-
-@Getter
-public class AuctionBidRequestDto {
-    private Long auctionPostId;
-    private int bidPrice;   // 입찰 금액
-}
--- backend/src/main/java/com/example/dukdang/entity/TradeStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.dukdang.entity;
-
-public enum TradeStatus {
-    SALE, // 판매중
-    RESERVED, // 예약중
-    SOLD // 거래완료
-}
